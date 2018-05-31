d49f77db6640cf402e1b36e1130da267de76e059
Carlos Fernandes <carlosf@motorola.com>
2018-05-30 21:36:49 -0300

    Calculate kmeans to k=5,10,15,20 for PCA with scale=true|false

--- INF-0613-final.R
+++ INF-0613-final.R
@@ -2,6 +2,21 @@
 # Trabalho Final - INF-0613
 # Nome(s): Carlos Fernandes e Liselene de Abreu Borges
 ########################################
+
+calculateKmeans <- function(dataset) {
+  K <- c(5,10,15,20)
+  x <- 1
+  means <- list()
+  for (k in K) {
+    print(k)
+    set.seed(1234)
+    means[[x]] <- kmeans(dataset,k, nstart = 20)
+    #print(means.scaled[[x]]$totss)
+    x <- x + 1
+  }
+  return (means)
+}
+
 rm(list=ls())
 setwd("/Carlos/ComplexData/INF-0613/Trabalho Final")
 #setwd("~/Projects/ComplexData/trabalho/INF-0613")
@@ -24,18 +39,8 @@ pc90<-a[1]
 dataset<-features.reduced$x[,1:1654]
 d<- dist(dataset)
 
-#kmeans: 20 clusters tem melhor betweengss/totss
-K <- c(5,10,15,20)
-x <- 1
-for (k in K) {
-  set.seed(1234)
-  c <- kmeans(dataset,k, nstart = 20)
-  print(k)
-  print(c$betweenss/c$totss)
-  x <- x + 1
-  #s<-silhouette(km$cluster,d)
-  #summary(s)$avg.width
-}
+#kmeans
+clusters.scaled <- calculateKmeans(dataset)
 
 # Not Scaled
 features.reduced.not_scaled <- prcomp(features)
@@ -49,5 +54,8 @@ a<-features.importance.not_scaled[3,features.importance.not_scaled[3,]>0.90]
 pc90.not_scaled<-a[1]
 
 #define o conjunto de dados reduzido e calcula a matrix de distancias, para 1390 componentes (85%)
-dataset.scale_false<-features.reduced.not_scaled $x[,1:1390]
+dataset.scale_false<-features.reduced.not_scaled$x[,1:1390]
 d.scale_false<- dist(dataset.scale_false)
+
+#kmeans
+clusters.scaled.false <- calculateKmeans(dataset.scale_false)
